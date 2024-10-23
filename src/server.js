c3bff5354107abeab382599459f26c631bb8b6cc
Kevin <kevingmez.98@gmail.com>
2024-10-23 13:29:40 -0500

    Commit con la estructura básica del proyecto y conexion a una BD mariaDB

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const sequelize = require('./conf/database'); // Importar la conexión 
+
+
+//Configuración aplicacion
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando correctamente');
+});
+
+// Escuchar peticiones en el puerto solicitado
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto:${PORT}`);
+});
